d97a17d36d7725668c83787a02759ff6166e0763
Douglas Oliveira <oliveirandouglas@gmail.com>
2021-11-17 21:06:02 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
-    return "Aplicação executando com sucesso!"
+    return "Douglas Oliveira - Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- test.py
+++ test.py
@@ -17,4 +17,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):                                                                                                                                                                             
         # verifica o retorno do conteudo da pagina                                                                                                                                                       
-        self.assertEqual(self.result.data.decode('utf-8'), "Aplicação executando com sucesso!")
+        self.assertEqual(self.result.data.decode('utf-8'), "Douglas Oliveira - Laboratório Pipeline DevOps")
