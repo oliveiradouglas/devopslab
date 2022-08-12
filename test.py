b091000f60ce587a243fc76061f2a317e0601586
Douglas Oliveira <oliveirandouglas@gmail.com>
2022-08-11 22:23:12 -0300

    deploy

--- test.py
+++ test.py
@@ -17,4 +17,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):                                                                                                                                                                             
         # verifica o retorno do conteudo da pagina                                                                                                                                                       
-        self.assertEqual(self.result.data.decode('utf-8'), "Douglas Oliveira - Laboratório Pipeline DevOps")
+        self.assertEqual(self.result.data.decode('utf-8'), "Douglas Oliveira - Laboratório Pipeline DevOps 11/08/2022")
