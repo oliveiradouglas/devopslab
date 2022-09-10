cbf22874c787aa32598106114b37e46cd377854e
Douglas Oliveira <oliveirandouglas@gmail.com>
2022-09-10 13:36:06 -0300

    Entrega final do projeto da fase 5

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Douglas Oliveira - Laboratório Pipeline DevOps - 10/09/2022"
+    return "Douglas Oliveira - Laboratório Pipeline DevOps - Entrega Final"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
